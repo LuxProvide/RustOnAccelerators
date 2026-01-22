962fd606b66e72737c513e4dfe04db1dacff0046
Emmanuel Kieffer <emmanuel.kieffer@lxp.lu>
2026-01-22 06:40:58 +0100

    Started rust-cuda

--- code/rust-cuda/kernels/src/lib.rs
+++ code/rust-cuda/kernels/src/lib.rs
@@ -2,8 +2,16 @@ use cuda_std::prelude::*;
 
 #[kernel]
 #[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
-pub unsafe fn vecadd(a: &[f32], b: &[f32], c: *mut f32) {
-    let idx = thread::index_1d() as usize;
+pub unsafe fn conv2d_gray_f32(input: &[f32], b: &[f32], c: *mut f32) {
+
+
+
+    let x = thread::thread_idx_x() as usize;
+    let y = thread::thread_idx_y() as usize;
+
+    let lx = thread::block_idx_x() as usize * TILE_SIZE + ty;
+    let ly = thread::block_idx_y() as usize * TILE_SIZE + tx;
+
     if idx < a.len() {
         let elem = unsafe { &mut *c.add(idx) };
         *elem = a[idx] + b[idx];
