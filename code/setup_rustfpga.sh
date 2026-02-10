b2681b5b03e3a47eb29e1201d1432b2d1a0da8b6
Emmanuel Kieffer <emmanuel.kieffer@lxp.lu>
2026-02-10 14:52:03 +0100

    Force release-2024.12

--- code/setup_rustfpga.sh
+++ code/setup_rustfpga.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -l
 
-check_mel3_node() {
-    local host
-    host=$(hostname -s)
+module --force purge
+module load env/release/2024.1
 
-    if [[ ! $host =~ ^mel3 ]]; then
-        echo "Error: This script must be sourced on an FPGA  node (current: $host)" >&2
-        exit 1
-    fi
+check_mel3_node() {
+  local host
+  host=$(hostname -s)
+
+  if [[ ! $host =~ ^mel3 ]]; then
+    echo "Error: This script must be sourced on an FPGA  node (current: $host)" >&2
+    exit 1
+  fi
 }
 
 check_mel3_node
-
 
 module load util-linux ifpgasdk 520nmx
 
@@ -20,19 +22,8 @@ export RUSTUP_HOME="$LOCALSCRATCH/${USER}/rustup"
 echo "Setting CARGO_HOME=${CARGO_HOME}"
 echo "Setting RUSTUP_HOME=${RUSTUP_HOME}"
 if [[ ! -f "${CARGO_HOME}/bin/rustup" ]]; then
- echo "Installing Rust"
- curl -sSf -L https://sh.rustup.rs | bash -s -- -y --no-modify-path --profile minimal --default-toolchain none
+  echo "Installing Rust"
+  curl -sSf -L https://sh.rustup.rs | bash -s -- -y --no-modify-path --profile minimal --default-toolchain none
 fi
 
 . "${CARGO_HOME}/env"
-
-
-
-
-
-
-
-
-
-
-
